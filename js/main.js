ed34cd2854d0ac1ae0f3c75bfcf7352b6619980b
Nicolas <lnicolasjofre@hotmail.com>
2023-03-26 06:13:36 -0300

    funcionamiento de hamburguer icon

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#btn-toggle');
+    menu_bar.addEventListener('click',function(){
+        menu.classList.toggle('btn-togler-event')
+    })
