ad671908066eca182c67601f831d751574622649
Nicolas <lnicolasjofre@hotmail.com>
2023-03-27 04:15:36 -0300

    agregue funcionalidad a clase active

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let menu = document.querySelector('#menu');
+let menu_container=document.getElementById('menu');
+let link = menu_container.getElementsByClassName("link");
+for(var i=0;i<link.length;i++){
+    link[i].addEventListener("click",function(){
+        var current=document.getElementsByClassName("active");
+        current[0].className=current[0].className.replace("active","");
+        this.className +=" active";
+    });
+}
 let menu_bar = document.querySelector('#btn-toggle');
     menu_bar.addEventListener('click',function(){
         menu.classList.toggle('btn-togler-event')
-    })
+    });
+
