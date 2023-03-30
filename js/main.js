b18a09cc9bea4d801dbdb816457df551809cb959
Nicolas <lnicolasjofre@hotmail.com>
2023-03-30 01:06:45 -0300

    cambio de lugar de codigo js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ let menu = document.querySelector('#menu');
 let menu_container=document.getElementById('menu');
 let link = menu_container.getElementsByClassName("link");
 let menu_bar = document.querySelector('#btn-toggle');
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+      } else {
+        document.getElementById("navbar").style.top = "-90px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+    
 
 for(var i=0;i<link.length;i++){
     link[i].addEventListener("click",function(){
@@ -10,9 +22,11 @@ for(var i=0;i<link.length;i++){
         this.className +=" active";
     });
 }
-
-
     menu_bar.addEventListener('click',function(){
         menu.classList.toggle('btn-togler-event')
     });
 
+    function myFunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+        }
